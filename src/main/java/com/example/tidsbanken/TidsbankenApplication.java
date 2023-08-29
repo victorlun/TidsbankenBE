1d33cc375308944da61a0fb7013819c967589658
victorlun <victor.lundqvist@se.experis.com>
2023-08-29 11:01:53 +0200

    Initial commit

---
+++ src/main/java/com/example/tidsbanken/TidsbankenApplication.java
@@ -0,0 +1,13 @@
+package com.example.tidsbanken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TidsbankenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TidsbankenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tidsbanken/TidsbankenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tidsbanken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TidsbankenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
