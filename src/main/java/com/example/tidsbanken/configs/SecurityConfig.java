5acab8163d95abe370b2313ecc87281940362d3a
victorlun <victor.lundqvist@se.experis.com>
2023-09-08 09:29:11 +0200

    fix: changed port and allowed swagger ui

--- src/main/java/com/example/tidsbanken/configs/SecurityConfig.java
+++ src/main/java/com/example/tidsbanken/configs/SecurityConfig.java
@@ -33,6 +33,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/v1/periods").permitAll()
                         .requestMatchers("/api/v1/blocked-periods").permitAll()
                         .requestMatchers("/api/v1/employees/**").permitAll()
+                        .requestMatchers("swagger-ui/**").permitAll()
                         .anyRequest().authenticated())
                 .oauth2ResourceServer((oauth2) -> oauth2
                         // Configure JWT-based authentication and sets a custom JWT authentication converter.
